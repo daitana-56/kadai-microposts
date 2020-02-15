b54037e69e6e092652f93102f1b2ea76b0344385
dai.tana <0108.sdkill@gmail.com>
2020-02-15 07:04:29 +0000

    user signup

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  #email小文字変換
+  before_save { self.email.downcase! }
+  
+  #バリデーション
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255},
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
+  
+  #パスワード用の設定
+  has_secure_password
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   #TOPページへのルーティング
   root to: 'toppages#index'
+  
+  #userのルーティング
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
